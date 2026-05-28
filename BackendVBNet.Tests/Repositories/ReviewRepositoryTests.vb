ce2024a60f4c52b4960c1ec4886549c9b25606ec
spentpark <spentpark@gmail.com>
2026-05-28 15:32:23 -0400

    test

--- BackendVBNet.Tests/Repositories/ReviewRepositoryTests.vb
+++ BackendVBNet.Tests/Repositories/ReviewRepositoryTests.vb
@@ -1,3 +1,4 @@
+Imports System
 Imports System.Collections.Generic
 Imports System.Threading.Tasks
 Imports Microsoft.EntityFrameworkCore
@@ -14,22 +15,19 @@ Public Class ReviewRepositoryTests
 
     <Fact>
     Public Async Function GetByGameIdAsync_ReturnsOnlyReviewsForGivenGame() As Task
-        ' Arrange
         Using context = CreateInMemoryContext()
             context.review.AddRange(
-                New Review With {.Id = 1, .GameId = 10, .Author = "Player1", .Score = "9", .Comment = "Excelente", .CreatedAt = DateTime.Now},
-                New Review With {.Id = 2, .GameId = 10, .Author = "Player2", .Score = "8", .Comment = "Muy bueno", .CreatedAt = DateTime.Now},
-                New Review With {.Id = 3, .GameId = 99, .Author = "Otro",    .Score = "5", .Comment = "Regular",   .CreatedAt = DateTime.Now}
+                New Review With {.Id = 1, .GameId = 10, .Author = "Player1", .Score = "9", .Comment = "Excelente",  .CreatedAt = DateTime.Now},
+                New Review With {.Id = 2, .GameId = 10, .Author = "Player2", .Score = "8", .Comment = "Muy bueno",  .CreatedAt = DateTime.Now},
+                New Review With {.Id = 3, .GameId = 99, .Author = "Otro",    .Score = "5", .Comment = "Regular",    .CreatedAt = DateTime.Now}
             )
             Await context.SaveChangesAsync()
 
             Dim repository = New ReviewRepository(context)
 
-            ' Act
             Dim result = Await repository.GetByGameIdAsync(10)
             Dim list = result.ToList()
 
-            ' Assert
             Assert.Equal(2, list.Count)
             Assert.All(list, Function(r) Assert.Equal(10, r.gameId))
         End Using
@@ -37,7 +35,6 @@ Public Class ReviewRepositoryTests
 
     <Fact>
     Public Async Function GetByGameIdAsync_ReturnsCorrectFields() As Task
-        ' Arrange
         Using context = CreateInMemoryContext()
             Dim createdAt = New DateTime(2024, 1, 15)
             context.review.Add(
@@ -54,11 +51,9 @@ Public Class ReviewRepositoryTests
 
             Dim repository = New ReviewRepository(context)
 
-            ' Act
             Dim result = Await repository.GetByGameIdAsync(10)
             Dim review = result.First()
 
-            ' Assert
             Assert.Equal(1, review.id)
             Assert.Equal(10, review.gameId)
             Assert.Equal("Player1", review.author)
@@ -70,7 +65,6 @@ Public Class ReviewRepositoryTests
 
     <Fact>
     Public Async Function GetByGameIdAsync_WhenNoReviewsForGame_ReturnsEmptyList() As Task
-        ' Arrange
         Using context = CreateInMemoryContext()
             context.review.Add(
                 New Review With {.Id = 1, .GameId = 99, .Author = "Player1", .Score = "9", .Comment = "Otro juego", .CreatedAt = DateTime.Now}
@@ -79,45 +73,42 @@ Public Class ReviewRepositoryTests
 
             Dim repository = New ReviewRepository(context)
 
-            ' Act
             Dim result = Await repository.GetByGameIdAsync(10)
 
-            ' Assert
             Assert.Empty(result)
         End Using
     End Function
 
     <Fact>
     Public Async Function GetByGameIdAsync_WhenDatabaseEmpty_ReturnsEmptyList() As Task
-        ' Arrange
         Using context = CreateInMemoryContext()
             Dim repository = New ReviewRepository(context)
 
-            ' Act
             Dim result = Await repository.GetByGameIdAsync(10)
 
-            ' Assert
             Assert.Empty(result)
         End Using
     End Function
 
     <Fact>
     Public Async Function GetByGameIdAsync_WhenMultipleGames_ReturnsOnlyRequestedGame() As Task
-        ' Arrange
         Using context = CreateInMemoryContext()
             context.review.AddRange(
-                New Review With {.Id = 1, .GameId = 1,  .Author = "A", .Score = "10", .Comment = "Juego 1", .CreatedAt = DateTime.Now},
-                New Review With {.Id = 2, .GameId = 2,  .Author = "B", .Score = "8",  .Comment = "Juego 2", .CreatedAt = DateTime.Now},
-                New Review With {.Id = 3, .GameId = 3,  .Author = "C", .Score = "6",  .Comment = "Juego 3", .CreatedAt = DateTime.Now},
-                New Review With {.Id = 4, .GameId = 2,  .Author = "D", .Score = "7",  .Comment = "Juego 2 otra review", .CreatedAt = DateTime.Now}
+                New Review With {.Id = 1, .GameId = 1, .Author = "A", .Score = "10", .Comment = "Juego 1",          .CreatedAt = DateTime.Now},
+                New Review With {.Id = 2, .GameId = 2, .Author = "B", .Score = "8",  .Comment = "Juego 2",          .CreatedAt = DateTime.Now},
+                New Review With {.Id = 3, .GameId = 3, .Author = "C", .Score = "6",  .Comment = "Juego 3",          .CreatedAt = DateTime.Now},
+                New Review With {.Id = 4, .GameId = 2, .Author = "D", .Score = "7",  .Comment = "Juego 2 otra review", .CreatedAt = DateTime.Now}
             )
             Await context.SaveChangesAsync()
 
             Dim repository = New ReviewRepository(context)
 
-            ' Act
             Dim result = Await repository.GetByGameIdAsync(2)
             Dim list = result.ToList()
 
-            ' Assert
             Assert.Equal(2, list.Count)
+            Assert.All(list, Function(r) Assert.Equal(2, r.gameId))
+        End Using
+    End Function
+
+End Class
